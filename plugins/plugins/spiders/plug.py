85efc9df40331bedd53e4d52ff8a93c328e840e0
Ludovic FERNEZ <ludo@fernez.fr>
2018-03-01 01:57:34 +0100

    extract sales page url.csv

--- plugins/plugins/spiders/plug.py
+++ plugins/plugins/spiders/plug.py
@@ -1,14 +1,25 @@
+# -*- coding: utf-8 -*-
+from scrapy.loader.processors import MapCompose, Join
+from scrapy.loader import ItemLoader
+from plugins.items import PluginsItem
+import datetime
+from urllib.parse import urlparse
+import socket
 import scrapy
+
 
 
 class NobPlugins(scrapy.Spider):
     name = "plugins"
-    start_urls = [
+    allowed_domains = ["nobuna.com"]
+    start_urls = (
         'https://www.nobuna.com/product-category/plugins/?product_count=20',
-    ]
+    )
+
 
     def parse(self, response):
-        for plugin in response.css('div.product-details'):
+        for plugin in response.css('div.product-details-container'):
             yield {
-                'text': plugin.css('h3.product-title::text').extract_first(),
+                'Sales_page_url': plugin.css('h3.product-title a::attr(href)').extract_first(),
+                'title': plugin.css('h3.product-title a::text').extract_first().strip(), 
             }
